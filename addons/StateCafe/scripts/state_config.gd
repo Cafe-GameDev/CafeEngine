47380edeec0b9dc68bf7ffe5898249285583b37b
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-06 15:07:16 -0300

    a

--- addons/StateCafe/scripts/state_config.gd
+++ addons/StateCafe/scripts/state_config.gd
@@ -2,6 +2,12 @@
 extends Resource
 class_name StateConfig
 
+@export var is_panel_expanded: bool = false:
+	set(value):
+		if is_panel_expanded != value:
+			is_panel_expanded = value
+			_save_and_emit_changed()
+
 signal config_changed
 
 func _save_and_emit_changed():
