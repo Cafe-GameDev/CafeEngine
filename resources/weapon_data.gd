cfa1a79ae8367e4ccdba4e059da2202dff9f7590
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-08 16:29:47 -0300

    f

---
+++ resources/weapon_data.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name WeaponData
+
+@export var weapon_name: String = "None"
+@export var attack_animation_name: String = ""
+@export var attack_sfx_path: String = ""
+@export var damage: float = 0.0
+@export var is_ranged: bool = false
+@export var projectile_scene_path: String = ""
+@export var sheathe_animation_name: String = ""
+@export var idle_animation_name: String = ""
+@export var run_animation_name: String = ""
