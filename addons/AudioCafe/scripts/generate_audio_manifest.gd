d05a0f909388c83a41fbaf17799e58b148e1b9b2
Cafe-GameDev <soarxz.games@gmail.com>
2025-09-15 20:42:05 -0300

    a

--- addons/AudioCafe/scripts/generate_audio_manifest.gd
+++ addons/AudioCafe/scripts/generate_audio_manifest.gd
@@ -1,6 +1,7 @@
 @tool
 extends EditorScript
 class_name GenerateAudioManifest
+
 signal progress_updated(current: int, total: int)
 signal generation_finished(success: bool, message: String)
 
@@ -21,27 +22,23 @@ func _run():
 	for path in audio_config.music_paths:
 		_count_files_in_directory(path)
 
-	print("Generating AudioManifest...")
+	print("Generating AudioManifest v1.1 with native playlists...")
 	
 	var audio_manifest = AudioManifest.new()
 	var success = true
 	var message = ""
 
 	for path in audio_config.sfx_paths:
-		if not _scan_and_populate_library(path, audio_manifest.sfx_data, "sfx"):
+		if not _scan_and_create_playlist(path, audio_manifest.sfx_data, "sfx"):
 			success = false
 			message = "Falha ao escanear caminhos SFX."
 			break
-		if not success:
-			break
-
-	for path in audio_config.music_paths:
-		if not _scan_and_populate_library(path, audio_manifest.music_data, "music"):
-			success = false
-			message = "Falha ao escanear caminhos de Música."
-			break
-		if not success:
-			break
+	if success:
+		for path in audio_config.music_paths:
+			if not _scan_and_create_playlist(path, audio_manifest.music_data, "music"):
+				success = false
+				message = "Falha ao escanear caminhos de Música."
+				break
 	
 	if success:
 		var error = ResourceSaver.save(audio_manifest, MANIFEST_SAVE_FILE)
@@ -49,15 +46,15 @@ func _run():
 			success = false
 			message = "Falha ao salvar AudioManifest.tres: %s" % error
 		else:
-			print("AudioManifest generated and saved to: %s" % MANIFEST_SAVE_FILE)
+			print("AudioManifest v1.1 gerado e salvo em: %s" % MANIFEST_SAVE_FILE)
 
 	emit_signal("generation_finished", success, message)
+
 
 func _count_files_in_directory(current_path: String):
 	var dir = DirAccess.open(current_path)
 	if not dir:
 		return
-
 	dir.list_dir_begin()
 	var file_or_dir_name = dir.get_next()
 	while file_or_dir_name != "":
@@ -67,7 +64,8 @@ func _count_files_in_directory(current_path: String):
 			_total_files_to_scan += 1
 		file_or_dir_name = dir.get_next()
 
-func _scan_and_populate_library(current_path: String, library: Dictionary, audio_type: String) -> bool:
+
+func _scan_and_create_playlist(current_path: String, library: Dictionary, audio_type: String) -> bool:
 	var dir = DirAccess.open(current_path)
 	if not dir:
 		printerr("GenerateAudioManifest: Failed to open directory: %s" % current_path)
@@ -77,40 +75,55 @@ func _scan_and_populate_library(current_path: String, library: Dictionary, audio
 	var file_or_dir_name = dir.get_next()
 	while file_or_dir_name != "":
 		if dir.current_is_dir():
-			if not _scan_and_populate_library(current_path.path_join(file_or_dir_name), library, audio_type):
+			if not _scan_and_create_playlist(current_path.path_join(file_or_dir_name), library, audio_type):
 				return false
 		elif file_or_dir_name.ends_with(".ogg") or file_or_dir_name.ends_with(".wav"):
 			_files_scanned += 1
 			emit_signal("progress_updated", _files_scanned, _total_files_to_scan)
 
 			var resource_path = current_path.path_join(file_or_dir_name)
-			var uid = ResourceLoader.get_resource_uid(resource_path)
-			print("  - Debug: Resource Path: %s, Raw UID: %s" % [resource_path, str(uid)])
-			if uid != -1:
-				var root_path_to_remove = ""
-				if audio_type == "sfx":
-					for p in audio_config.sfx_paths:
-						if resource_path.begins_with(p) and p.length() > root_path_to_remove.length():
-							root_path_to_remove = p
-				else:
-					for p in audio_config.music_paths:
-						if resource_path.begins_with(p) and p.length() > root_path_to_remove.length():
-							root_path_to_remove = p
+			var audio_stream: AudioStream = load(resource_path)
+			if audio_stream == null:
+				printerr("Falha ao carregar AudioStream: %s" % resource_path)
+				file_or_dir_name = dir.get_next()
+				continue
 
-				var relative_dir_path = resource_path.replace(root_path_to_remove, "").trim_prefix("/").get_base_dir().trim_suffix("/")
-				var final_key = ""
+			var root_path_to_remove = ""
+			if audio_type == "sfx":
+				for p in audio_config.sfx_paths:
+					if resource_path.begins_with(p) and p.length() > root_path_to_remove.length():
+						root_path_to_remove = p
+			else:
+				for p in audio_config.music_paths:
+					if resource_path.begins_with(p) and p.length() > root_path_to_remove.length():
+						root_path_to_remove = p
 
-				if not relative_dir_path.is_empty():
-					final_key = relative_dir_path.replace("/", "_").to_lower()
-				else:
-					final_key = file_or_dir_name.get_basename().to_lower()
+			var relative_dir_path = resource_path.replace(root_path_to_remove, "").trim_prefix("/").get_base_dir().trim_suffix("/")
+			var final_key = ""
+			if not relative_dir_path.is_empty():
+				final_key = relative_dir_path.replace("/", "_").to_lower()
+			else:
+				final_key = file_or_dir_name.get_basename().to_lower()
 
-				if not library.has(final_key):
-					library[final_key] = []
-				library[final_key].append("%s" % str(uid))
-				print("  - Added %s audio to playlist '%s' with UID: %s" % [audio_type, final_key, str(uid)])
+			var playlist_file_path = "%s%s_playlist.tres" % [DIST_SAVE_PATH, final_key]
+			var playlist: AudioStreamPlaylist
+			if FileAccess.file_exists(playlist_file_path):
+				playlist = load(playlist_file_path)
+				if not playlist:
+					playlist = AudioStreamPlaylist.new()
+			else:
+				playlist = AudioStreamPlaylist.new()
+
+			var streams_array = playlist.get("streams")
+			if streams_array == null:
+				streams_array = []
+			streams_array.append(audio_stream)
+			playlist.set("streams", streams_array)
+
+			if playlist_file_path != "":
+				ResourceSaver.save(playlist, playlist_file_path)
+				library[final_key] = playlist_file_path
+				print("  - Added %s audio to playlist '%s' at: %s" % [audio_type, final_key, playlist_file_path])
 		file_or_dir_name = dir.get_next()
-	return true
 
-func _get_uid_from_import_file(import_file_path: String) -> String:
-	return ""
+	return true
